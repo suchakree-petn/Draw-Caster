345a6df4740071080ddcc3647a45bbca187d2291
Nor_Book <thanawat.srit@kmutt.ac.th>
2023-08-24 22:21:42 +0700

    EDIT: Script Spell

--- Assets/Spell/Spell.cs
+++ Assets/Spell/Spell.cs
@@ -6,23 +6,25 @@ public class Spell : ScriptableObject
 {
     public string _name;
     public string _description;
-    public SpellType _spellType;
-    public Sprite _icon;
     public int _level;
     public float _cooldown;
     public float _manaCost;
+    public Sprite _icon;
+    public SpellType _spellType;
     public SpellElement _spellElement;
     public KeyCode _activateKey;
+    public virtual void Attack(){}
 }
 public enum SpellType{
+    Default,
     QuickCast,
     Consentrade
 }
-
 public enum SpellElement{
+    Default,
     Fire,
     Thunder,
     Frost,
     Wind
-
 }
+
