f940f669b8eead89f9caa5fd6b93bf62e80a5390
Nor_Book <thanawat.srit@kmutt.ac.th>
2023-08-24 18:32:46 +0700

    PULL: Tutor To Thanva

---
+++ Assets/Player/Scripts/PlayerManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public PlayerData playerData;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
