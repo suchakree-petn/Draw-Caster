e5801869aa0ede201149cfed8ea3efee4b5fda4d
Nor_Book <thanawat.srit@kmutt.ac.th>
2023-08-25 00:09:17 +0700

    EDIT: Script PlayerManager

--- Assets/Player/Scripts/PlayerManager.cs
+++ Assets/Player/Scripts/PlayerManager.cs
@@ -5,12 +5,27 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour, IDamageable
 {
     public PlayerData playerData;
+    public Rigidbody2D _playerRb;
 
-    public void TakeDamage(Elemental elementalDamage)
+    public void TakeDamage(Elemental damage)
     {
         if (playerData.HealthPoint > 0)
         {
-            // playerData.HealthPoint -= elementalDamage._damage; เปลี่ยนเป็น CalcDefense ก่อน ค่อยเอามาหักเลือด
+            playerData.HealthPoint -= CalcDamageRecieve(playerData, damage);
         }
+    }
+    float CalcDefense(CharactorData target)
+    {
+        return (target._defenseBase * (1 + target._defenseMultiplier)) + target._defenseBonus;
+    }
+
+    float CalcDMGReduction(CharactorData target,Elemental damage)
+    {
+        float _targetDefense = CalcDefense(target);
+        return 1-(_targetDefense / (_targetDefense + (5 * damage._attacker._level) +500));
+    }
+    float CalcDamageRecieve(CharactorData target, Elemental damage)
+    {
+        return damage._damage * CalcDMGReduction(target, damage) * (1 - target._bonusDamageReduction);
     }
 }
