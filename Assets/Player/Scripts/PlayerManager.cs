de9a99f76abc293ea69df84f154bb5e767b33a4c
suchakree-petn <suchakree.petn@kmutt.ac.th>
2023-09-04 23:21:52 +0700

    ADD: Add damage receive calculator function to player manager

--- Assets/Player/Scripts/PlayerManager.cs
+++ Assets/Player/Scripts/PlayerManager.cs
@@ -7,27 +7,47 @@ public class PlayerManager : MonoBehaviour, IDamageable
 
     public PlayerData playerData;
     public Rigidbody2D _playerRb;
+    public float currentHp;
 
+    private void Start()
+    {
+        currentHp = playerData._maxHp;
+    }
     public void TakeDamage(Elemental damage)
     {
-        if (playerData._currentHp > 0)
+        if (currentHp > 0)
         {
-            playerData._currentHp -= CalcDamageRecieve(playerData, damage);
+            float damageDeal = CalcDamageRecieve(playerData, damage);
+            currentHp -= damageDeal;
+            DamagePopup.CreateTextDamage(transform.position, damageDeal, damage._elementalType);
         }
-        
+        GameController.OnPlayerTakeDamage?.Invoke(gameObject);
     }
     float CalcDefense(CharactorData target)
     {
         return (target._defenseBase * (1 + target._defenseMultiplier)) + target._defenseBonus;
     }
 
-    float CalcDMGReduction(CharactorData target,Elemental damage)
+    float CalcDMGReduction(CharactorData target, Elemental damage)
     {
         float _targetDefense = CalcDefense(target);
-        return 1-(_targetDefense / (_targetDefense + (5 * damage._attacker._level) +500));
+        return 1 - (_targetDefense / (_targetDefense + (5 * damage._attacker._level) + 500));
     }
     float CalcDamageRecieve(CharactorData target, Elemental damage)
     {
         return damage._damage * CalcDMGReduction(target, damage) * (1 - target._bonusDamageReduction);
+    }
+    public void CalcMaxHp()
+    {
+        currentHp = playerData._maxHp;
+    }
+
+    private void OnEnable()
+    {
+        GameController.OnBeforeStart += CalcMaxHp;
+    }
+    private void OnDisable()
+    {
+        GameController.OnBeforeStart -= CalcMaxHp;
     }
 }
