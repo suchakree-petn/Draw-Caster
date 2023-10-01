74d490b7f05a3f5b613bcc1a8dfe890255162eff
suchakree-petn <suchakree.petn@kmutt.ac.th>
2023-10-02 06:18:25 +0700

    FEAT: Add new Flame pillar attack

---
+++ Assets/Charactor/Enemy/MoltenFire/Attack/CheckCollider/CheckFireBallAttack.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using MoltenFire;
+using UnityEngine;
+
+public class CheckFireBallAttack : MonoBehaviour
+{
+    [SerializeField] private MoltenFireBehaviour moltenFireBehaviour;
+    [SerializeField] private float activeDuration;
+    private void OnEnable()
+    {
+        Debug.Log("fireBal");
+        StartCoroutine(ActiveDuration());
+    }
+    private void Awake()
+    {
+        moltenFireBehaviour = transform.root.GetComponentInChildren<MoltenFireBehaviour>();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.root.CompareTag(moltenFireBehaviour.targetTag) && other.CompareTag("Hitbox"))
+        {
+            moltenFireBehaviour.currentState = State.FireBallAttack;
+            gameObject.SetActive(false);
+
+        }
+    }
+    IEnumerator ActiveDuration()
+    {
+        yield return new WaitForSeconds(activeDuration);
+        moltenFireBehaviour.currentState = State.Wandering;
+        Debug.Log(moltenFireBehaviour.currentState);
+        gameObject.SetActive(false);
+    }
+}
